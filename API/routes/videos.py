304ce0ba6fe4e96ad1aa9c13f0b753a6a6c9875c
TotallyLe1gitAccount <tllylgitacnut@gmail.com>
2026-06-29 12:39:14 +0300

    refactor: add Annotated to FastAPI dependencies for clarity

--- API/routes/videos.py
+++ API/routes/videos.py
@@ -1,17 +1,27 @@
+import os
+from typing import Annotated
 from ..core.dependencies import get_video_service, get_video_repo
 from fastapi import APIRouter, UploadFile, HTTPException, File, Depends
 from data.core import get_db
 from sqlalchemy.orm import Session
-import os
+from data.crud import CRUD as VideoRepo
+from API.services.video_service import VideoService
 
 router = APIRouter(prefix='/video')
 
 @router.get('/')
-def show_videos(db: Session = Depends(get_db), repo = Depends(get_video_repo)):
+def show_videos(
+    db: Annotated[Session, Depends(get_db)], 
+    repo: Annotated[VideoRepo, Depends(get_video_repo)]
+    ):
     return repo.get_all_videos(db)
 
 @router.get('/{video_id}')
-def show_video(video_id: int, db: Session = Depends(get_db), repo = Depends(get_video_repo)):
+def show_video(
+    video_id: int, 
+    db: Annotated[Session, Depends(get_db)], 
+    repo: Annotated[VideoRepo, Depends(get_video_repo)]
+    ):
     video = repo.get_video(db, video_id=video_id)
 
     if not video:
@@ -22,14 +32,19 @@ def show_video(video_id: int, db: Session = Depends(get_db), repo = Depends(get_
     }
 
 @router.post('/upload')
-async def upload_video(
-    file: UploadFile = File(...), 
-    db: Session = Depends(get_db),
-    service = Depends(get_video_service)):
-    return await service.upload_video(file, db)
+async def upload_video( 
+    db: Annotated[Session, Depends(get_db)],
+    service: Annotated[VideoService, Depends(get_video_service)],
+    file: UploadFile = File(...)
+    ):
+    return await service.upload_video(db, file)
 
 @router.delete('/{video_id}')
-def del_video(video_id: int, db: Session = Depends(get_db), repo = Depends(get_video_repo)):
+def del_video(
+    video_id: int, 
+    db: Annotated[Session, Depends(get_db)], 
+    repo: Annotated[VideoRepo, Depends(get_video_repo)]
+    ):
     video = repo.get_video(db, video_id)
 
     if not video:
