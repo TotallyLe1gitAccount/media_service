93c2d4571498420d32fdf5c1c0a0ab29b5ba8175
TotallyLe1gitAccount <tllylgitacnut@gmail.com>
2026-07-31 16:15:13 +0300

    feat: add verify and hash password methods

--- auth/security.py
+++ auth/security.py
@@ -2,7 +2,7 @@ import jwt
 from pwdlib import PasswordHash
 import secrets
 from config import (
-    SECRET_KEY, ALGORHITM,
+    SECRET_KEY, ALGORITHM,
     ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_DAYS)
 import datetime
 
@@ -13,16 +13,29 @@ def generate_access_token(token_data: dict) -> str:
     expired = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode.update({"exp": expired})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORHITM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
 
     return encoded_jwt
 
 def decode_token(token: str):
     try:
-        jwt.decode(token, SECRET_KEY, algorithms=[ALGORHITM])
+        payload = jwt.decode(
+            token, 
+            SECRET_KEY, 
+            algorithms=[ALGORITHM])
 
-    except jwt.DecodeError:
+        return payload
+
+    except Exception as e:
+        print("JWT ERROR:", repr(e))
         return None
+    
+#PASSWORD 
+password_hash = PasswordHash.recommended() 
 
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
 
-
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+    
